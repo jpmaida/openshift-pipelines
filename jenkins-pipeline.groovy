cd79187b11a6396a2379e579f56afaaa004ff897
João Paulo Maida <jpmaida@gmail.com>
2020-10-25 16:15:58 -0300

    New steps

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -15,5 +15,46 @@ pipeline {
 	  			}
 	  		}
 	  	}
+	  	stage('Build') {
+			steps {
+				script {
+					echo "Clone GIT URL: ${env.APP_GIT_URL} da branch: ${env.APP_GIT_BRANCH}"
+					git branch: env.APP_GIT_URL, url: env.APP_GIT_BRANCH
+					echo "Clone realizado com sucesso"
+
+					def pom = readMavenPom file: "${env.POM_FILE_PATH}"
+					def version = pom.version
+					echo "Definido versão para uma aplicação maven. Versão: ${version}"
+
+					sh "mvn clean install -DskipTests=true -f ${env.POM_FILE_PATH}"
+				}
+			}
+		}
+		stage('Test') {			
+			steps {
+				script{
+					sh "mvn test -f ${env.POM_FILE_PATH}"
+				}
+			}
+		}
+		stage('Code Analysis') {
+			steps {
+				script {
+					try{
+						def sonarqubeUrl = "https://sonarqube-cicd.apps.sharedocp311cns.lab.upshift.rdu2.redhat.com"
+						sh "mvn sonar:sonar -Dsonar.host.url=${sonarqubeUrl} -DskipTests=true -Dsonar.scm.provider=git -f ${pomFilePath}"
+					} catch(Exception e) {
+						echo "Erro ao chamar o Sonar. - " + e.getMessage()
+					}
+				}
+			}
+		}
+		stage('Deploy') {
+			steps {
+				script {
+					echo "Deployyyy"
+				}
+			}
+		}
     }
 }
