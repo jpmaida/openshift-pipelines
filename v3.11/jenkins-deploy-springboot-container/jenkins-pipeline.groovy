4df03f90e304382da898594f300fb5d9cbd288d0
João Paulo Maida <jpmaida@gmail.com>
2021-07-26 13:50:09 -0300

    Troca de rota hard coded

--- v3.11/jenkins-deploy-springboot-container/jenkins-pipeline.groovy
+++ v3.11/jenkins-deploy-springboot-container/jenkins-pipeline.groovy
@@ -41,7 +41,7 @@ pipeline {
 			steps {
 				script {
 					try{
-						def sonarqubeUrl = "https://sonarqube-cicd.apps.na311.openshift.opentlc.com "
+						def sonarqubeUrl = "<route_to_sonarqube>"
 						sh "mvn sonar:sonar -Dsonar.host.url=${sonarqubeUrl} -DskipTests=true -Dsonar.scm.provider=git -f ${env.POM_FILE_PATH}"
 					} catch(Exception e) {
 						echo "Erro ao chamar o Sonar. - " + e.getMessage()
